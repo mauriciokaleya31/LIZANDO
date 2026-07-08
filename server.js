da79c620591316d57dcca2b638db0f9f8f42275a
Mauricio Kaleya <149623707+mauriciokaleya31@users.noreply.github.com>
2026-07-08 23:43:55 +0100

    1

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor em execução na porta ${PORT}`);
+});
